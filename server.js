04585c3d8e2691f965b9c8ecfbd33f28e79be9e5
Jake <jake83@gmail.com>
2016-03-28 12:38:24 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.send('');
+// });
+
+app.listen(port);
+
+console.log('Server listing on port ' + port);
