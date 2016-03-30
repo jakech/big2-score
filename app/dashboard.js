5b37f3904ea65da2b704242f42c9000cf85c4c68
Jake <jake83@gmail.com>
2016-03-29 17:52:33 -0700

    reset focus on the first input after submit

--- app/dashboard.js
+++ app/dashboard.js
@@ -74,3 +74,23 @@ angular.module('big2score.dashboard')
     }
   };
 });
+
+// sync input focus
+angular.module('big2score.dashboard')
+  .directive('syncFocusWith', function($timeout, $rootScope) {
+    return {
+      restrict: 'A',
+      scope: {
+        focusValue: "=syncFocusWith"
+      },
+      link: function($scope, $element, attrs) {
+        $scope.$watch("focusValue", function(currentValue, previousValue) {
+          if (currentValue === true && !previousValue) {
+            $element[0].focus();
+          } else if (currentValue === false && previousValue) {
+            $element[0].blur();
+          }
+        })
+      }
+    }
+});
